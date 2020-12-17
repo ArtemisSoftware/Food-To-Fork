6ce3697dc9d67429a732b310de98b8a1f77a708f
gustavo <gustavo.isaac@gmail.com>
2020-12-17 12:29:06 +0000

    Hilt Dependency Injection

---
+++ app/src/main/java/com/artemissoftware/foodtofork/FoodtoforkApplication.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.foodtofork
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodtoforkApplication : Application(){
+}
