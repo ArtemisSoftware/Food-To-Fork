5d838ea4fa6133425d0f9abc228a2428e9145dbf
gustavo <gustavo.isaac@gmail.com>
2020-12-22 10:27:03 +0000

    Persisting data in ViewModels with MutableState

--- app/src/main/java/com/artemissoftware/foodtofork/ui/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/com/artemissoftware/foodtofork/ui/recipeList/RecipeListViewModel.kt
@@ -1,10 +1,27 @@
 package com.artemissoftware.foodtofork.ui.recipeList
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.artemissoftware.foodtofork.domain.model.Recipe
 import com.artemissoftware.foodtofork.repository.RecipeRepository
+import kotlinx.coroutines.launch
 import javax.inject.Named
 
 class RecipeListViewModel @ViewModelInject constructor(private val repository: RecipeRepository,
                                                        private @Named("auth_token") val token: String) : ViewModel(){
+
+
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
+
+    init{
+
+        viewModelScope.launch {
+            val result = repository.search(token = token, page = 1, query = "chicken")
+            recipes.value = result
+        }
+    }
+
 }
---
+++ app/src/main/java/com/artemissoftware/foodtofork/util/Constants.kt
@@ -0,0 +1,3 @@
+package com.artemissoftware.foodtofork.util
+
+const val TAG = "AppDebug"
